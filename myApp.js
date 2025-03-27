046f72a6b544d09799625e8e130dc88f0cc3057f
John Keno Iweh <iwehjohn@gmail.com>
2025-03-27 20:55:15 +0000

    sent static css file over api

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
 
 const homePagePath = __dirname + '/views/index.html';
+const homestyle = __dirname + '/public';
+
+app.use("/public",express.static(homestyle));
+
 app.get("/", (req, res) => {
     res.sendFile(homePagePath);
 });
+
 
 
 
